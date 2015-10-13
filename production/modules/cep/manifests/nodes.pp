336203ab9ebbf824f9c2f7cadb7248f029ab9a01
sajith <sajithshn@gmail.com>
2015-10-13 16:15:43 +0530

    Initial files
    Signed-off-by: sajith <sajithshn@gmail.com>

---
+++ production/modules/cep/manifests/nodes.pp
@@ -0,0 +1,31 @@
+$common 	= hiera("nodeinfo")
+$datasource = hiera("datasources")
+node /hanode/ {
+	$hanode = hiera("hanode")
+	class { "cep::hanode":
+		version 		    		=> $common[version],
+		owner               		=> $common[owner],
+        group               		=> $common[group],
+        maintenance_mode			=> $common[maintenance_mode],
+  		offset  		    		=> $hanode[offset],
+  		clustering          		=> $hanode[clustering],
+  		depsync						=> $hanode[depsync],
+  		local_member_port   		=> $hanode[local_member_port],
+  		membership_scheme   		=> $hanode[membership_scheme],
+  		cluster_domain      		=> $hanode[cluster_domain],
+  		members             		=> $hanode[members],
+  		worker              		=> $hanode[worker],
+  		presenter           		=> $hanode[presenter],
+  		ha_eventsynch_port  		=> $hanode[ha_eventsynch_port],
+  		ha_mgt_port         		=> $hanode[ha_mgt_port],
+  		presenter_port 				=> $hanode[presenter_port],
+  		registry_db_connection_url  => $datasource[registry_db_connection_url],
+        registry_db_user            => $datasource[registry_db_user],
+        registry_db_password        => $datasource[registry_db_password],
+        registry_db_driver_name     => $datasource[registry_db_driver_name],
+        userstore_db_connection_url	=> $datasource[userstore_db_connection_url],
+        userstore_db_user           => $datasource[userstore_db_user],
+        userstore_db_password       => $datasource[userstore_db_password],
+        userstore_db_driver_name    => $datasource[userstore_db_driver_name],
+	}
+}
